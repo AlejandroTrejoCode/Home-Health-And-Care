d7a5129ba877de881c60104bb6da1942520ac500
Mac-01 <Chicle-1@iMac-de-Mac-01.local>
2018-03-03 11:07:16 -0500

    funcion resize para el menu

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 
 document.getElementById("menu").checked = false;
 new WOW().init();
+var menu = document.getElementById("menu");
 
 function menuClick(){
-    var menu = document.getElementById("menu");
     if (menu.checked == true) {
             document.getElementById("header-bottom-right").style.display = "flex"; 
         } else {
             document.getElementById("header-bottom-right").style.display = "none";
         }
+}
+
+function resize(){
+	if(window.innerWidth >= 576){
+		document.getElementById("header-bottom-right").style.display = "flex";
+	}
+	else {
+            document.getElementById("header-bottom-right").style.display = "none";
+		
+	}
 }
